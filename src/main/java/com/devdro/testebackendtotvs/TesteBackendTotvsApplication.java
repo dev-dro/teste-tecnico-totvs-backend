8655924db8f0dcb8c4ba712b8144617e6b4b1f93
Danilo Oliveira <danilo.ro@protonmail.com>
2022-06-17 10:09:49 -0300

    Create the project using spring initializr

---
+++ src/main/java/com/devdro/testebackendtotvs/TesteBackendTotvsApplication.java
@@ -0,0 +1,13 @@
+package com.devdro.testebackendtotvs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteBackendTotvsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TesteBackendTotvsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/devdro/testebackendtotvs/TesteBackendTotvsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdro.testebackendtotvs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteBackendTotvsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
